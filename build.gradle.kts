54789dd511bbba81eb827e3ab33915aa04737652
purofle <purofle@gmail.com>
2023-02-27 23:56:51 +0800

    jpa 依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ extra["snippetsDir"] = file("build/generated-snippets")
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
