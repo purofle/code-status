d0315f6a3640910339ec14d8ed4b8c5db90c7fa8
purofle <purofle@gmail.com>
2023-04-19 23:55:49 +0800

    chore: update SpringBoot and Kotlin

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.3"
+	id("org.springframework.boot") version "3.0.5"
 	id("io.spring.dependency-management") version "1.1.0"
     id("com.apollographql.apollo3") version "3.8.0"
-	kotlin("jvm") version "1.8.0"
-	kotlin("plugin.spring") version "1.8.0"
-	kotlin("plugin.jpa") version "1.8.0"
+	kotlin("jvm") version "1.8.20"
+	kotlin("plugin.spring") version "1.8.20"
+	kotlin("plugin.jpa") version "1.8.20"
 }
 
 group = "tech.archlinux"
